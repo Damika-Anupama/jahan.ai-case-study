451115c5b0bd2fcf26c59291cf7c7be8558159d0
Damika-Anupama <damikaanupama@gmail.com>
2024-11-16 08:21:19 +0530

    authenticate works

--- user_preferences_backend/preferences/auth.py
+++ user_preferences_backend/preferences/auth.py
@@ -4,8 +4,11 @@ from django.contrib.auth.hashers import check_password
 def authenticate(email, password):
     try:
         user = AccountSettings.objects.get(email=email)
-        if check_password(password, user.password):
+        print (user.password, password)
+        if user.password == password:
             return user
+        # if check_password(password, user.password):
+        #     return user
     except AccountSettings.DoesNotExist:
         return None
     return None
